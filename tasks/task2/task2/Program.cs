66bd85977d932d6f7fd2f9077bfd9fcfda3d339c
Bernhard Brückler <bernhard@brueckler.co>
2016-05-17 22:42:48 +0200

    task 3.

--- tasks/task2/task2/Program.cs
+++ tasks/task2/task2/Program.cs
@@ -6,14 +6,25 @@ using System.Threading.Tasks;
 
 namespace task2
 {
-    public class Bike
+    interface Bike
+    {
+        string Manufacturer { get; }
+        string Model { get; }
+        int ServiceInterval { get; set; }
+
+        int GetMileage();
+        int GetNextService();
+        void SetMileage(int new_mileage);
+    }
+
+    public class Sportbike : Bike
     {
         private int m_mileage;
         public string Manufacturer { get; }
         public string Model { get; }
         public int ServiceInterval { get; set; }
 
-        public Bike(string manuf, string model, int mileage)
+        public Sportbike(string manuf, string model, int mileage)
         {
             if (string.IsNullOrWhiteSpace(manuf)) throw new ArgumentException("Manufacturer must not be empty.", nameof(manuf));
             if (string.IsNullOrWhiteSpace(model)) throw new ArgumentException("Model must not be empty.", nameof(model));
@@ -41,19 +52,60 @@ namespace task2
         }
     }
 
+    public class Dirtbike : Bike
+    {
+        private int m_mileage;
+        public string Manufacturer { get; }
+        public string Model { get; }
+        public int ServiceInterval { get; set; }
+        public int ServiceInterval_Oil { get; set; }
+
+        public Dirtbike(string manuf, string model, int mileage)
+        {
+            if (string.IsNullOrWhiteSpace(manuf)) throw new ArgumentException("Manufacturer must not be empty.", nameof(manuf));
+            if (string.IsNullOrWhiteSpace(model)) throw new ArgumentException("Model must not be empty.", nameof(model));
+            if (mileage < 0) throw new ArgumentOutOfRangeException("Mileage must not be negative. ", nameof(mileage));
+
+            Manufacturer = manuf;
+            Model = model;
+            m_mileage = mileage;
+        }
+
+        public int GetNextService()
+        {
+            return ServiceInterval - (m_mileage % ServiceInterval);
+        }
+
+        public int GetNextService_Oil()
+        {
+            return ServiceInterval - (m_mileage % ServiceInterval_Oil);
+        }
+
+        public void SetMileage(int new_mileage)
+        {
+            if (new_mileage < m_mileage) throw new ArgumentOutOfRangeException("New mileage may not be less than the current mileage. ");
+            m_mileage = new_mileage;
+        }
+
+        public int GetMileage()
+        {
+            return m_mileage;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var myBike = new Bike("Yamaha", "MT-07", 13000);
-            myBike.ServiceInterval = 10000;
+            var bikes = new Bike[] {
+                new Sportbike ("Yamaha", "MT-07", 13000),
+                new Dirtbike ("Yamaha", "WR250", 4500)
+            };
 
-            Console.WriteLine("Data for my " + myBike.Manufacturer + " " + myBike.Model + ": ");
-            Console.WriteLine("Current mileage: " + myBike.GetMileage() + "km. Next maintenance in: " + myBike.GetNextService() + "km.");
-            int newMileage = 23460;
-            Console.WriteLine("Setting mileage to " + newMileage + "km.");
-            myBike.SetMileage(newMileage);
-            Console.WriteLine("Current mileage: " + myBike.GetMileage() + "km. Next maintenance in: " + myBike.GetNextService() + "km.");
+            foreach (var b in bikes)
+            {
+                Console.WriteLine(b.Manufacturer + " " + b.Model + ": KM-Stand " + b.GetMileage() + " km.");
+            }
         }
     }
 }
