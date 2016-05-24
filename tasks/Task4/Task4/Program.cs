e73c0a595544a2260292f37603d4c30f0c3e49f4
Bernhard Brückler <bernhard@brueckler.co>
2016-05-24 17:11:36 +0200

    task 4, serialization funktioniert nicht

---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+    interface Bike
+    {
+        string Manufacturer { get; }
+        string Model { get; }
+        int ServiceInterval { get; set; }
+
+        int GetMileage();
+        int GetNextService();
+        void SetMileage(int new_mileage);
+    }
+
+    public class Sportbike : Bike
+    {
+        private int m_mileage;
+        public string Manufacturer { get; }
+        public string Model { get; }
+        public int ServiceInterval { get; set; }
+
+        public Sportbike(string manuf, string model, int mileage)
+        {
+            if (string.IsNullOrWhiteSpace(manuf)) throw new ArgumentException("Manufacturer must not be empty.", nameof(manuf));
+            if (string.IsNullOrWhiteSpace(model)) throw new ArgumentException("Model must not be empty.", nameof(model));
+            if (mileage < 0) throw new ArgumentOutOfRangeException("Mileage must not be negative. ", nameof(mileage));
+
+            Manufacturer = manuf;
+            Model = model;
+            m_mileage = mileage;
+        }
+
+        public int GetNextService()
+        {
+            return ServiceInterval - (m_mileage % ServiceInterval);
+        }
+
+        public void SetMileage(int new_mileage)
+        {
+            if (new_mileage < m_mileage) throw new ArgumentOutOfRangeException("New mileage may not be less than the current mileage. ");
+            m_mileage = new_mileage;
+        }
+
+        public int GetMileage()
+        {
+            return m_mileage;
+        }
+    }
+
+    public class Dirtbike : Bike
+    {
+        private int m_mileage;
+        public string Manufacturer { get; }
+        public string Model { get; }
+        public int ServiceInterval { get; set; }
+        public int ServiceInterval_Oil { get; set; }
+
+        public Dirtbike(string manuf, string model, int mileage)
+        {
+            if (string.IsNullOrWhiteSpace(manuf)) throw new ArgumentException("Manufacturer must not be empty.", nameof(manuf));
+            if (string.IsNullOrWhiteSpace(model)) throw new ArgumentException("Model must not be empty.", nameof(model));
+            if (mileage < 0) throw new ArgumentOutOfRangeException("Mileage must not be negative. ", nameof(mileage));
+
+            Manufacturer = manuf;
+            Model = model;
+            m_mileage = mileage;
+        }
+
+        public int GetNextService()
+        {
+            return ServiceInterval - (m_mileage % ServiceInterval);
+        }
+
+        public int GetNextService_Oil()
+        {
+            return ServiceInterval - (m_mileage % ServiceInterval_Oil);
+        }
+
+        public void SetMileage(int new_mileage)
+        {
+            if (new_mileage < m_mileage) throw new ArgumentOutOfRangeException("New mileage may not be less than the current mileage. ");
+            m_mileage = new_mileage;
+        }
+
+        public int GetMileage()
+        {
+            return m_mileage;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bikes = new Bike[] {
+                new Sportbike ("Yamaha", "MT-07", 13000),
+                new Dirtbike ("Yamaha", "WR250", 4500)
+            };
+
+            bikes[0].ServiceInterval = 10000;
+            bikes[1].ServiceInterval = 5000;
+
+            var settings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var filename = Path.Combine(desktop, "bikes.json");
+            var json_strings = JsonConvert.SerializeObject(bikes, settings);
+
+            File.WriteAllText(filename, json_strings);
+
+            // Read JSON from file
+            var json_text = File.ReadAllText(filename);
+            var json_items = JsonConvert.DeserializeObject<Bike[]>(json_text, settings);
+
+            // Print deserialized objects
+            foreach (var b in json_items)
+            {
+                Console.WriteLine(b.Manufacturer + " " + b.Model + ": KM-Stand " + b.GetMileage() + " km.");
+            }
+        }
+    }
+}
---
+++ tasks/Task4/Task4/Tests_Task4.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task4
+{
+    [TestFixture]
+    class Tests_Task4
+    {
+        [Test]
+        public void ManufacturerMustNotBeEmpty()
+        {
+            Assert.Catch(() =>
+            {
+                var newBike = new Sportbike("", "Model", 9);
+            });
+        }
+
+        [Test]
+        public void ModelMustNotBeEmpty()
+        {
+            Assert.Catch(() =>
+            {
+                var newBike = new Sportbike("Manufacturer", "", 9);
+            });
+        }
+
+        [Test]
+        public void MileageMustNotBeNegative()
+        {
+            Assert.Catch(() =>
+            {
+                var newBike = new Sportbike("M", "M", -9);
+            });
+        }
+
+        [Test]
+        public void BikePropertiesSameAfterInitialisation()
+        {
+            var newBike = new Sportbike("Manufacturer", "Model", 123456);
+
+            Assert.IsTrue(newBike.Manufacturer == "Manufacturer");
+            Assert.IsTrue(newBike.Model == "Model");
+            Assert.IsTrue(newBike.GetMileage() == 123456);
+        }
+
+        [Test]
+        public void BikeMileageCanBeIncreased()
+        {
+            var newBike = new Sportbike("Manufacturer", "Model", 10000);
+            newBike.SetMileage(15000);
+            Assert.IsTrue(newBike.GetMileage() == 15000);
+        }
+
+        [Test]
+        public void BikeMileageCannotBeDecreased()
+        {
+            var newBike = new Sportbike("Manufacturer", "Model", 10000);
+           
+            Assert.Catch(() =>
+            {
+                newBike.SetMileage(5000);
+            });
+        }
+
+        [Test]
+        public void GetNextServiceReturnsCorrectValue()
+        {
+            var newBike = new Sportbike("Manufacturer", "Model", 10000);
+            newBike.ServiceInterval = 3000;
+            Assert.IsTrue(newBike.GetNextService() == 2000);
+        }
+
+        [Test]
+        public void DirtbikePropertiesAreSettable()
+        {
+            var newDirtbike = new Dirtbike("Manufacturer", "Model", 20000);
+            newDirtbike.ServiceInterval = 5000;
+            newDirtbike.ServiceInterval_Oil = 2000;
+
+            Assert.IsTrue(newDirtbike.ServiceInterval == 5000);
+            Assert.IsTrue(newDirtbike.ServiceInterval_Oil == 2000);
+        }
+    }
+}
